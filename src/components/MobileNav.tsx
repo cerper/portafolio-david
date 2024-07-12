c5d6b1543ac54ac1e40560e7d4470ffb74e23814
root <root@DESKTOP-ATFCIID>
2024-07-12 11:56:28 -0400

    (editando) mobile nav

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,29 +1,29 @@
 import { Link as ScrollLink } from 'react-scroll'
 
 const linkData = [
-  { name: 'home', target: 'home', offset: 0 },
-  { name: 'habilidades', target: 'habilidades', offset: 0 },
-  { name: 'proyectos', target: 'proyectos', offset: 0 },
-  { name: 'contacto', target: 'contacto', offset: 0 },
+  { name: 'home', target: 'home', offset: -100 },
+  { name: 'habilidades', target: 'habilidades', offset: -120 },
+  { name: 'proyectos', target: 'proyectos', offset: -140 },
+  { name: 'contacto', target: 'contacto', offset: -120 },
 ]
 const MobileNav = ({ containerStyles }: { containerStyles: string }) => {
   return (
-      <nav className={`${containerStyles}`}>
-        {linkData.map((link, index) => {
-          return (
-            <ScrollLink
-              key={index}
-              offset={link.offset}
-              to={link.target}
-              spy
-              smooth
-              className="cursor-pointer transition-all hover:text-blue-700"
-            >
-              {link.name}
-            </ScrollLink>
-          )
-        })}
-      </nav>
+    <nav className={`${containerStyles}`}>
+      {linkData.map((link, index) => {
+        return (
+          <ScrollLink
+            key={index}
+            offset={link.offset}
+            to={link.target}
+            spy
+            smooth
+            className="cursor-pointer transition-all hover:text-blue-700"
+          >
+            {link.name}
+          </ScrollLink>
+        )
+      })}
+    </nav>
   )
 }
 export default MobileNav
